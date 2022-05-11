a7b0b49b5e2b86603805ef636c9ebe45c787e6c1
eminencedevs <victoribidunmoye@gmail.com>
2022-05-11 04:20:48 +0000

    added a for loop

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ const button = () => {
   console.log(3 * 5)
   alert('Quiz for the day; what is 23 x 12?')
 }
+let count = 5
+for (let index = 0; index < count; index++) {
+  console.log('I shall be great')
+}
